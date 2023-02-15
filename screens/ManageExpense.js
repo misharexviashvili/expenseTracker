b054887b76fb5d032a36f8df8994b48f0084d853
Misha <misharexviashvili@yahoo.com>
2023-02-15 22:19:45 +0400

    cancel and update buttons added

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { StyleSheet, View } from "react-native";
 import { useLayoutEffect } from "react";
 import IconbButton from "../components/UI/IconbButton";
 import GlobalStyles from "../constants/styles";
+import Button from "../components/UI/Button";
 
 const ManageExpense = ({ navigation, route }) => {
   // params must be checked with ? because if its undefined drilling it will cause error
@@ -19,8 +20,18 @@ const ManageExpense = ({ navigation, route }) => {
   });
   // TODO:
   function deleteExpenseHandler() {}
+  function cancelHandler() {}
+  function confirmHandler() {}
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconbButton
@@ -41,6 +52,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
