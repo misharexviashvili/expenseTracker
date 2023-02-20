155cdb8bef5b338b5fa1cfe35d85e9daf93df171
Misha <misharexviashvili@yahoo.com>
2023-02-20 20:36:11 +0400

    spinning loader added when app starts

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,18 +1,25 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 // Elements will render here if they are actually 7 days old max
 const RecentExpenses = () => {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesCtx = useContext(ExpensesContext);
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
   // const expensesCtx = useContext(ExpensesContext);
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
